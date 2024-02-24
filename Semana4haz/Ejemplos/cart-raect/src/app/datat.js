f1e587c1b51f6e05fc887e228245cc18e0c4a3c6
haze452va <125218780+haze452va@users.noreply.github.com>
2024-02-23 20:47:11 -0600

    Add files via upload

---
+++ Semana4haz/Ejemplos/cart-raect/src/app/datat.js
@@ -0,0 +1,82 @@
+export const data=[
+    {
+    id:1,
+    title:'Cien años de soledad',
+    price:100,
+    urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
+    descripcion:'Cien años de soledad es una novela del escritor colombiano Gabriel García Márquez, ganador del Premio Nobel de Literatura en 1982. Es considerada una obra maestra de la literatura hispanoamericana y universal, así como una de las obras más traducidas y leídas en español.',
+    quantity:1
+    },
+    {
+    id:2,
+    title:'El señor de los anillos(Trilogía)',
+    price:190,
+    urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg',
+    descripcion:'La trilogía de "El Señor de los Anillos" La gran epopeya de Tolkien consta de tres volúmenes, en los que seguiremos la peripecia de nuestros héroes para destruir el Anillo Único. Para descubrirlo, debes leer la trilogía en este orden cronológico: La comunidad del Anillo, Las Dos Torres y El Retorno del Rey.',
+    quantity:1
+    },
+    {
+    id:3,
+    title:'Cuentos de Barro',
+    price:30,
+    urlImage:'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+    descripcion:'Los relatos que conforman Cuentos de barro, la primera obra literaria de Salarrué, se centran en la población rural del El Salvador, su vulnerabilidad, su superstición o la brutalidad que rige sus vidas. En su escritura, el autor refleja el habla local propia de los campesinos de El Salvador.',
+    quantity:1
+    },
+    {
+    id:4,
+    title:'Tierra de Infancia',
+    price:30,
+    urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+    descripcion:'Tierra de infancia (Libro). Esta obra forma parte de la historia autobiográfica de la poeta salvadoreña Claudia Lars, en la década de los 30 en los tiempos de la dictadura del general Martínez en los sucesos del 2 de abril de 1944. Esta historia se da en las faldas del Volcán de Izalco donde una niña relata sus historias de alegría y tristeza.',
+    quantity:1
+    },
+    {
+    id:5,
+    title:'Harry Potter Pack',
+    price:390,
+    urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+    descripcion:'Harry Potter es una colección de las siete novelas de la saga más famosa de la literatura fantástica, escrita por J.K. Rowling. Los libros narran las aventuras de Harry Potter, un joven mago que estudia en el Colegio Hogwarts de Magia y Hechicería, y sus amigos Ron Weasley y Hermione Granger. Juntos se enfrentan al malvado lord Voldemort, que busca la inmortalidad y el poder absoluto.',
+    quantity:1
+    },
+    {
+        id:6,
+        title:'Eleanor & Park',
+        price:25,
+        urlImage:'https://th.bing.com/th/id/OIP.cQCpD5zCwnTqTg9PEpydzwHaLV?rs=1&pid=ImgDetMain',
+        descripcion:'Eleanor y Park es una novela escrita por Rainbow Rowell perteneciente a la literatura juvenil, que fue por primera vez publicada en febrero de 2013. La historia se sitúa en los años 80, y gira en torno a la relación que surge entre dos compañeros de colegio, Eleanor Douglas y Park Sheridan, en la localidad norteamericana de Omaha, Nebraska.',
+        quantity:1
+    },
+    {
+        id:7,
+        title:'Rojo, Blanco y Sangre Azul',
+        price:15,
+        urlImage:'https://th.bing.com/th/id/OIP.x6HH7c9D1gESw_qo_u3CngHaLX?rs=1&pid=ImgDetMain',
+        descripcion:'es una novela romántica LGBT de 2019 escrita por Casey McQuiston. La novela se centra en el personaje de Alex Claremont-Diaz, hijo latino de la presidenta de los Estados Unidos, y su relación romántica con Henry, un príncipe inglés.',
+        quantity:1
+    },
+    {
+        id:8,
+        title:'Perdona si te llamo amor',
+        price:10,
+        urlImage:'https://th.bing.com/th/id/OIP.rxzsR4eywK7NRoxfaWDeSAHaLN?rs=1&pid=ImgDetMain',
+        descripcion:'Niki es una joven madura y responsable que cursa su último año de secundaria. Alessandro es un exitoso publicista de treinta y siete años a quien acaba de dejar su novia de toda la vida. A pesar de los veinte años de diferencia que hay entre ambos y del abismo generacional que los separa, Niki y Alessandro se enamorarán locamente y vivirán una apasionada historia de amor en contra de todas las convenciones y prejuicios sociales.',
+        quantity:1
+    },
+    {
+        id:9,
+        title:'La chica del tren',
+        price:20,
+        urlImage:'https://imagessl1.casadellibro.com/a/l/s7/71/9788408141471.webp',
+        descripcion:'Rachel toma siempre el tren de las 8.04 h. Cada mañana lo mismo: el mismo paisaje, las mismas casas…y la misma parada en la señal roja. Son solo unos segundos, pero le permiten observar a una pareja desayunando tranquilamente en su terraza. Siente que los conoce y se inventa unos nombres para ellos: Jess y Jason. Su vida es perfecta, no como la suya. Pero un día ve algo. Sucede muy deprisa, pero es suficiente. ¿Y si Jess y Jason no son tan felices como ella cree? ¿Y si nada es lo que parece? Tú no la conoces. Ella a ti, sí.',
+        quantity:1
+    },
+    {
+        id:10,
+        title:'Prohibido',
+        price:30,
+        urlImage:'https://imagessl0.casadellibro.com/a/l/s7/60/9788417525460.webp',
+        descripcion:'Lochan y Maya de diecisiete años, siempre se han sentido más amigos que hermanos. Ante la incapacidad de una madre alcohólica y la ausencia de un padre que los abandonó, deben hacerse cargo de sus hermanos menores y esconder su situación a los servicios sociales.  Esa responsabilidad les ha unido, tanto, que se han enamorado.  Saben que su relación está mal y que no puede continuar, pero al mismo tiempo no pueden controlar sus emociones y la atracción les domina.  Su amor es un amor prohibido, y si alguien descubre su secreto, no habrá un final feliz para ellos.',
+        quantity:1
+    },
+    ];
